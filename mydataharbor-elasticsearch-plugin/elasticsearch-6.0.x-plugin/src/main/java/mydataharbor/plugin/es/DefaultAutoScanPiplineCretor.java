9f312a7ca1b9b345c0414d3606263364e4f69149
xulang <xulang@doba.com>
2023-01-17 16:48:21 +0800

    适配2.x主框架

--- mydataharbor-elasticsearch-plugin/elasticsearch-6.0.x-plugin/src/main/java/mydataharbor/plugin/es/DefaultAutoScanPiplineCretor.java
+++ mydataharbor-elasticsearch-plugin/elasticsearch-6.0.x-plugin/src/main/java/mydataharbor/plugin/es/DefaultAutoScanPiplineCretor.java
@@ -1,18 +1,19 @@
 package mydataharbor.plugin.es;
 
-import mydataharbor.IDataPipline;
-import mydataharbor.plugin.base.creator.AbstractAutoScanPiplineCreator;
+import mydataharbor.IDataPipeline;
+import mydataharbor.plugin.base.creator.AbstractAutoScanPipelineCreator;
 import mydataharbor.setting.BaseSettingContext;
-import org.pf4j.Extension;
-import org.pf4j.ExtensionPoint;
 
 import java.util.Map;
+
+import org.pf4j.Extension;
+import org.pf4j.ExtensionPoint;
 
 /**
  * Created by xulang on 2021/8/17.
  */
 @Extension
-public class DefaultAutoScanPiplineCretor extends AbstractAutoScanPiplineCreator<Map<String, Object>, BaseSettingContext>implements ExtensionPoint {
+public class DefaultAutoScanPiplineCretor extends AbstractAutoScanPipelineCreator<Map<String, Object>, BaseSettingContext> implements ExtensionPoint {
   @Override
   public String scanPackage() {
     return "mydataharbor";
@@ -24,12 +25,12 @@ public class DefaultAutoScanPiplineCretor extends AbstractAutoScanPiplineCreator
   }
 
   @Override
-  public IDataPipline createPipline(Map<String, Object> config, BaseSettingContext settingContext) throws Exception {
-    throw new RuntimeException("改创建器无法创建pipline");
+  public IDataPipeline createPipeline(Map<String, Object> config, BaseSettingContext settingContext) throws Exception {
+    throw new RuntimeException("此创建器无法创建pipline");
   }
 
   @Override
-  public boolean canCreatePipline() {
+  public boolean canCreatePipeline() {
     return false;
   }
 }
